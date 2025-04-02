6f7da2552fd0ac8bfb735265c38b3baababef514
hjuneja99 <junejah0509@gmail.com>
2025-04-02 17:59:31 +0530

    Delete gemini20-voicetext/gemini20-voicetext directory

--- gemini20-voicetext/gemini20-voicetext/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start the HTTP server to serve static files (e.g., index.html) on port 8000
-python3 -m http.server 8000 &
-
-echo "HTTP server started on port 8000, serving static files."
-
-# Start the WebSocket server (this will run in the foreground)
-python main.py
